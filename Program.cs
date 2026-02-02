d444fa98bc5f1f0e9084686bee68a09db6b963ad
Nilima Pawase <158264249+nilimapawase@users.noreply.github.com>
2026-02-03 00:36:27 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace GitForConflict
         static void Main(string[] args)
         {
             int a=10;
-            int b=a+A;
+            int b=a+a;
             Console.Writeline(b);
         }
     }
